7d41dfa33763bc2b1ea0b540034ea1e5e6f06ed1
Donderstal <daanonderstal@hotmail.com>
2019-01-25 09:15:39 +0100

    solved double event firing

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 textObject = {
-    content1: '<h4 onclick=handleClick(event) class="blackText">Alles goed?<i class="fas fa-times float-right"></i></h4><br>Een wraaksausje kent vele aromas. Een beetje een peper, een beetje zout. Een beetje waar je al dan niet van houdt.',
-    content2: '<h4 onclick=handleClick(event) class="blackText">Lorem ipsum<i class="fas fa-times float-right"></i></h4><ul><li> dolor sit amet, consectetur adipiscing elit</li> <li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>',
-    content3: '<h4 onclick=handleClick(event) class="blackText">Soundcloud M4ddn3zz<i class="fas fa-times float-right"></i></h4><iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fuser-508800509%2Fzak-aardappels-demo&color=orange_white&size=32" style="width: 32px; height: 32px;"></iframe>',
-    content4: '<h4 onclick=handleClick(event) class="blackText">Lorem ipsum<i class="fas fa-times float-right"></i></h4>Toen onze Mop een Mopje was<br>Was hij aardig om te zien<br>Nu bromt hij alle dagen<br>En bijt nog bovendien<br>Waf-woef, waf-woef<br>Waf-woef, waf-woef<br>En bijt nog bovendien<br>Nu bromt hij alle dagen<br>En bijt nog bovendien',
-    row1div1: '<h4 id="content1" onclick=handleClick(event) class="grow2">Wat is een Wraaksaus?</h4>',
-    row1div2: '<h4 id="content2" onclick=handleClick(event) class="grow2">Agenda</h4>',
-    row2div1: '<h4 id="content3" onclick=handleClick(event) class="grow2">Muziek</h4>',
-    row2div2: "<h4 id='content4' onclick=handleClick(event) class='grow2'>Appeltaart...</h4>"
+    content1: '<h4 class="blackText">Alles goed?<i class="fas fa-times float-right"></i></h4><br>Een wraaksausje kent vele aromas. Een beetje een peper, een beetje zout. Een beetje waar je al dan niet van houdt.',
+    content2: '<h4 class="blackText">Lorem ipsum<i class="fas fa-times float-right"></i></h4><ul><li> dolor sit amet, consectetur adipiscing elit</li> <li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>',
+    content3: '<h4 class="blackText">Soundcloud M4ddn3zz<i class="fas fa-times float-right"></i></h4><iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fuser-508800509%2Fzak-aardappels-demo&color=orange_white&size=32" style="width: 32px; height: 32px;"></iframe>',
+    content4: '<h4 class="blackText">Lorem ipsum<i class="fas fa-times float-right"></i></h4>Toen onze Mop een Mopje was<br>Was hij aardig om te zien<br>Nu bromt hij alle dagen<br>En bijt nog bovendien<br>Waf-woef, waf-woef<br>Waf-woef, waf-woef<br>En bijt nog bovendien<br>Nu bromt hij alle dagen<br>En bijt nog bovendien',
+    row1div1: '<h4 id="content1" class="grow2">Wat is een Wraaksaus?</h4>',
+    row1div2: '<h4 id="content2" class="grow2">Agenda</h4>',
+    row2div1: '<h4 id="content3" class="grow2">Muziek</h4>',
+    row2div2: "<h4 id='content4' class='grow2'>Appeltaart...</h4>"
 }
 
 function getSlideshow() {
@@ -35,13 +35,20 @@ function handleClick(event) {
     if (event.target.id === "headerText") {
         closeSlideshow()
     }
-    else if (event.target.className === "grow2" /* || event.target.className.includes( "mainTextDivs" ) */){
+    else if (event.target.className === "grow2"){
         openTab(event.path, event.target) 
     }
+    else if (event.target.className.includes( "mainTextDivs" )) {
+        const evPath = event.path
+        evPath.shift()
+        openTab(evPath, event.target)
+    }
     else if (event.target.className === "blackText"){
+        console.log('heey')
         closeTab(event.path)
     }
     else if (event.target.className === "fas fa-times float-right"){
+        console.log('hyee')
         const evPath = event.path
         evPath.shift()
         closeTab(evPath)
@@ -63,36 +70,54 @@ function closeTab(eventPath){
 }
 
 function openTab(eventPath, eventTarget){
-    console.log($('.mainTextDivs'))
     let ID
-    let className
     if (event.path[0].className === "grow2") {
         ID = eventPath[1].id
-        className = eventPath[2].className
         $(eventTarget).wrap( "<div class='bg-light col-md-6 col-10 p-4 m-4'></div>" );        
         $(eventTarget).remove()    
     }
     else { 
-        ID = eventPath[0].id
-        className = eventPath[1].className
-        $(eventTarget).wrap( "<div class='bg-light col-md-6 col-10 p-4 m-4'></div>" );      
+        ID = eventTarget.id
+        $(Array.from(eventTarget.children)[0]).wrap( "<div class='bg-light col-md-6 col-10 p-4 m-4'></div>" );
     }
 
     $('#' + ID).attr( "class" , "wide p-5 mb-2" );
+    assortContent(ID)
+    getBottomTabs(eventPath)
+}
 
-    if (className.includes('row1')) {
-        ((ID.includes('div1'))
-        ?   addContent(ID, textObject.content1)
-        :   addContent(ID, textObject.content2)
-        )
+function getBottomTabs (eventPath) {
+    let divArray = Array.from(document.getElementsByClassName('mainTextDivs'))
+    divArray = sortDivArray(divArray, eventPath)
+    console.log(divArray)
+}
+
+function sortDivArray(divArray, eventPath) {
+    if (eventPath[1].className.includes('mainTextDivs')) {
+        const ID = eventPath[1].id
+        return divArray.filter( e => e.id != ID)
     }
-    else if (className.includes('row2')) {
-        ((ID.includes('div1'))
-        ?   addContent(ID, textObject.content3)
-        :   addContent(ID, textObject.content4)
-        )
+    else if (eventPath[0].className.includes('mainTextDivs')) {
+        const ID = eventPath[0].id
+        return divArray.filter( e => e.id != ID)
+    }    
+}
+
+function assortContent(ID) {
+    if ((ID.includes('row1div1'))) {
+        addContent(ID, textObject.content1)
+    }
+    else if ((ID.includes('row1div2'))) { 
+        addContent(ID, textObject.content2)
+    }
+    else if (ID.includes('row2div1')) {
+        addContent(ID, textObject.content3)
+    }  
+    else if (ID.includes('row2div2')) {
+        addContent(ID, textObject.content4)
     }
 }
+
 
 function addContent(ID, content) {
     $('div#'+ID+'>div.bg-light').html(content)
